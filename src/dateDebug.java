6c5e8eddf3528b3e2c2c6a6a2f581538999c545c
Cale146 <cale.d.jones@gmail.com>
2017-03-03 10:29:44 -0500

    dateDebug addition

---
+++ src/dateDebug.java
@@ -0,0 +1,23 @@
+/**
+ * Created by jonesdc on 1/31/17.
+ */
+public class dateDebug {
+
+    static final String LOG = "Trump_WIDGET";
+    private static final long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
+    private static final long HOUR_IN_MILLIS = 1000 * 60 * 60;
+    private static final long MINUTE_IN_MILLIS = 1000 * 60;
+
+    public static void main(String args[])
+    {
+        dateDebug getTheDate = new dateDebug();
+        getTheDate.trump();
+    }
+
+
+
+    public long trump(){
+        return 1;
+    }
+
+}
